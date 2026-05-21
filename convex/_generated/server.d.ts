ddb8bb1c85f03e2bfa4d9dae0fe2844bf6fbd884
kissik-dom <legal@kissikingdom.com>
2026-05-21 06:55:50 +0000

    chore: Add Convex generated stubs for Vite build

---
+++ convex/_generated/server.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+export { query, mutation, action, internalQuery, internalMutation, internalAction, httpAction } from "./server.js";
+export type QueryCtx = any;
+export type MutationCtx = any;  
+export type ActionCtx = any;
