6795074e30059c762e5e40a9111638094d6cfa45
kissik-dom <legal@kissikingdom.com>
2026-05-21 06:55:45 +0000

    chore: Add Convex generated stubs for Vite build

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable */
+import type { FilterApi, FunctionReference } from "convex/server";
+export declare const api: any;
+export declare const internal: any;
+export declare const components: any;
